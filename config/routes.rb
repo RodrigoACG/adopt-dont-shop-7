0ada5c1b019807dc8c34c57c6c5728c283e5bdec
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-03-30 16:30:46 -0600

    us 5 complete

--- config/routes.rb
+++ config/routes.rb
@@ -40,4 +40,7 @@ Rails.application.routes.draw do
   get "/applications/new", to: "applications#new"
   get "/applications/:id", to: "applications#show"
   post "/applications", to: "applications#create"
+  post "/applications/:id/pets", to: "application_pets#create"
+  
+
 end
