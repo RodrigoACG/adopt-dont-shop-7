d0f7d98cc2c861a129cea5816d39d1515c0078c4
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-03-30 14:39:20 -0600

    add Us 4 tests

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -46,4 +46,38 @@ RSpec.describe "Application show page" do
       expect(page).to have_content("Name of Shelter: Rescue Puppy")
     end
   end
+
+  describe '#us 4' do
+    it 'Searching for pets for an application' do
+      shelter1 = Shelter.create!(foster_program: true, name: "Adopt a Pet", city: "Denver", rank: 5 )
+      shelter2 = Shelter.create!(foster_program: true, name: "Rescue Puppy", city: "Los angeles", rank: 5 )
+
+      pet1 = shelter1.pets.create!(adoptable: true, age: 1, breed: "Dobermann", name: "Chop") 
+      pet2 = shelter1.pets.create!(adoptable: false, age: 6, breed: "Poodle", name: "Princess") 
+      pet3 = shelter2.pets.create!(adoptable: true, age: 3, breed: "Rottweiler", name: "Pantera") 
+      pet4 = shelter2.pets.create!(adoptable: true, age: 1, breed: "Goldendoodle", name: "Lucky") 
+
+      applicant1 = pet4.applications.create!(name: "Tyara", street_address: "1234 Washington st", city: "Los Angeles", state: "California", zip_code: 90028, description: "Very loving person")
+      applicant2 = pet3.applications.create!(name: "Clarisa", street_address: "1234 Washington st", city: "Los Angeles", state: "California", zip_code: 90029, description: "Has one and looking for another and wanted one since a kid.")
+      applicant3 = pet1.applications.create!(name: "Martin", street_address: "4532 Washington st", city: "Denver", state: "Colorado", zip_code: 80000, description: "Will treat dog as its own kid")
+      applicant4 = pet2.applications.create!(name: "Ezequiel", street_address: "4532 Washington st", city: "Denver", state: "Colorado", zip_code: 80001, description: "Has had one in the past and alway treated them with love")
+
+      # As a visitor
+      # When I visit an application's show page
+      visit "/applications/#{applicant1.id}"
+      # And that application has not been submitted,
+      # Then I see a section on the page to "Add a Pet to this Application"
+      expect(page).to have_content("Add a Pet to this Application:")
+      # In that section I see an input where I can search for Pets by name
+      expect(page).to have_field(:search)
+      # When I fill in this field with a Pet's name
+      fill_in :search, with: "Chop"
+      # And I click submit,
+      click_on("Submit")
+      expect(current_path).to eq("/applications/#{applicant1.id}")
+      expect(page).to have_content("Chop")
+      # Then I am taken back to the application show page
+      # And under the search bar I see any Pet whose name matches my search
+    end
+  end
 end
