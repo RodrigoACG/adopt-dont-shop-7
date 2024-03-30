36901f5970007fce14f9665e91b8ecda4102edb1
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-03-30 12:01:57 -0600

    Add the validate tests

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,5 +5,12 @@ RSpec.describe Application, type: :model do
     it { should have_many :application_pets} 
     it { should have_many(:pets).through(:application_pets)}
     
+    
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :street_address }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :state }
+    it { should validate_numericality_of :zip_code }
+    it { should validate_presence_of :description }
   end
 end
