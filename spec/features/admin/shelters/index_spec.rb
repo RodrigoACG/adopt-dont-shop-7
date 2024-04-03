5206303e6b2f7c096c4a436a482f96a125030fe2
Dana Howell <howelld115@gmail.com>
2024-04-03 07:47:00 -0600

    begins to fill out test for displaying shelters with pending apps

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -7,13 +7,28 @@ RSpec.describe 'admin shelter index page', type: :feature do
         @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
         @shelter_2 = Shelter.create(name: "RGV animal shelter", city: "Harlingen, TX", foster_program: false, rank: 5)
         @shelter_3 = Shelter.create(name: "Fancy pets of Colorado", city: "Denver, CO", foster_program: true, rank: 10)
+        require 'pry' ; binding.pry
+
+        pet1 = shelter1.pets.create!(adoptable: true, age: 1, breed: "Dobermann", name: "Chop") 
+        pet2 = shelter1.pets.create!(adoptable: false, age: 6, breed: "Poodle", name: "Princess") 
+        pet3 = shelter2.pets.create!(adoptable: true, age: 3, breed: "Rottweiler", name: "Pantera") 
+
+        applicant1 = Application.create!(name: "Tyara", street_address: "1234 Washington st", city: "Los Angeles", state: "California", zip_code: 90028, description: "Very loving person")
+        applicant2 = Application.create!(name: "Clarisa", street_address: "1234 Washington st", city: "Los Angeles", state: "California", zip_code: 90029, description: "Has one and looking for another and wanted one since a kid.")
+
+        
+
+        visit "/admin/shelters"
 
       end
       
       it 'displays shelters in reverse alphabetical order' do
-        visit "/admin/shelters"
         expect(@shelter_2.name).to appear_before(@shelter_3.name)
         expect(@shelter_3.name).to appear_before(@shelter_1.name)
+      end
+
+      it "displays shelters with pending applications" do
+        
       end
     end 
   end
