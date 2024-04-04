a2323fa310a9e1b633c52022b2979fe159d4322f
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-04-03 23:50:16 -0600

    remove comments

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -2,16 +2,8 @@ class Admin::ApplicationsController < ApplicationController
 
   def show 
     @application = Application.find(params[:id])
-    # if @application.status =! "Accepted"
-      
-    # end
+   
   end
 
-  # def update
-
-  #   require 'pry'; binding.pry
-  #   app_pet = ApplicationPets.find_by(application_id: params[:application_id], pet_id: params[:pet_id])
-  #   app_pet.update(status: params[:status])
-  #   redirect_to "/admin/applications/#{params[:application.id]}"
-  # end
+ 
 end
